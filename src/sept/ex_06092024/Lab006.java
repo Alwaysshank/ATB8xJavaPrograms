1283e53905628d7202525a6ca989c74753f4a368
Shashank <erukullashashank7@gmail.com>
2025-10-10 12:47:28 +0530

    Hello 123

---
+++ src/sept/ex_06092024/Lab006.java
@@ -0,0 +1,15 @@
+//Below code is package: folder with Sept-> ex_06 folder
+package sept.ex_06092024;
+/*Author: Shashank
+*
+* Write anything here
+*
+ **/
+//Program Java with the class file,name of the class is Lab006
+public class Lab006 {
+    //This is the main function,JVM starts from here
+    public static void main(String[] args) {
+        //Below code will be executed,it will print hello world
+        System.out.println("Hello World");
+    }
+}
